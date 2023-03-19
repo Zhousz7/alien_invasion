945b67302e6dcd95bbfbabb0dd4921c18721cd5f
Zhousz7 <2630015080@qq.com>
2023-03-19 20:07:28 +0800

    创建设置类

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,29 +1,30 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """"the class which control gaming resources and actions"""
     def __init__(self):
         """initialize the game and create gaming resources"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
-        pyagme.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
 
-        # set the background color
-        self.bg_color = (230,230,230)
 
     def run_game(self):
         """start the main circulation"""
         while True:
             # monitor the keyboard and mouse event
             for event in pygame.event.get():
-                if event.type == pygame.QUiT:
+                if event.type == pygame.QUIT:
                     sys.exit()
 
             # fill the screen after every circulation
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # make the lately screen visible
             pygame.display.flip()
