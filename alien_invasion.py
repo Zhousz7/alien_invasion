b6e81905eb8a0758d05ac927f9bddf15273c1b37
Zhousz7 <2630015080@qq.com>
2023-03-19 21:36:17 +0800

    驾驶飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,31 +12,45 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
-
 
     def run_game(self):
         """start the main circulation"""
         while True:
             self._check_events()
-
-            # fill the screen after every circulation
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # make the lately screen visible
-            pygame.display.flip()
-
+            self.ship.update()
+            self._update_screen()
 
     def _check_events(self):
         """response for keyboard and mouse events"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
+    def _check_keydown_events(self, event):
+        """response for keydown"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """response for keyup"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """update the image and switch to the new screen"""
@@ -44,6 +58,7 @@ class AlienInvasion:
         self.ship.blitme()
 
         pygame.display.flip()
+
 
 if __name__ == '__main__':
     # create the game example and run the game
--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self,ai_game):
         """initialize the ship and its biginning position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # load the ship image and get its bounding rectangle
@@ -13,6 +14,24 @@ class Ship:
 
         # for every new ship,put it on the middle of bottom of screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # store x with decimal numbers
+        self.x = float(self.rect.x)
+
+        # moving signal
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update ship's position according to the moving signal"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # update the rect object according to self.x
+        self.rect.x = self.x
+
 
     def blitme(self):
         """draw the ship on giving position"""
